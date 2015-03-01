c62731a7ae10f02d39b220e4f67697c47329e06a
nrmnr <n.pujita+github@gmail.com>
2015-03-01 18:31:26 +0900

    Sequence クラス導入テスト

--- kakikudashi.rb
+++ kakikudashi.rb
@@ -6,9 +6,14 @@
 # http://kou.benesse.co.jp/nigate/japanese/a13j0306.html
 
 class Kunten
+  RE_KUNTEN = /^([^ァ-ヶ\[\(]+)([ァ-ヶ]*)?(\([ァ-ヶ]*\))?(\[.*\])?$/
+
   def initialize node, seq
-    @kanji, @kana, @kaeri = node.scan(/^([^ァ-ヶ\[\]]+)([ァ-ヶ]+)?(?:\[(.+)\])?$/).flatten.map(&:to_s)
-    raise to_s if @kaeri =~ /^\|/ and !@kana.empty?
+    raise node unless node =~ RE_KUNTEN
+    @kanji, @kana, @sai, @kaeri = node.scan(RE_KUNTEN).flatten.map(&:to_s)
+    @sai.gsub! /[\(\)]/, ""
+    @kaeri.gsub! /[\[\]]/, ""
+    raise node if @kaeri =~ /^\|/ and !@kana.empty?
     @seq = seq
     @read_times = 0
   end
@@ -19,20 +24,8 @@ class Kunten
     @read_times += 1
   end
 
-  def to_raw
-    if @kaeri.empty?
-      "#{@kanji}#{@kana}"
-    else
-      "#{@kanji}#{@kana}[#{@kaeri}]"
-    end
-  end
-
   def to_s
-    if @kanji =~ /[。、]/
-      @kanji
-    else
-      "%s(%s)[%s]" % [@kanji, @kana, @kaeri]
-    end
+    "%s%s(%s)[%s]" % [@kanji, @kana, @sai, @kaeri]
   end
 
   def to_kan_furi index
@@ -91,17 +84,50 @@ class Kunten
     return false
   end
 
+  def saidoku
+    @sai.tr("ァ-ン", "ぁ-ん")
+  end
+
   def furigana
     @kana.tr("ァ-ン", "ぁ-ん")
   end
 end
 
+class Sequence
+  include Enumerable
+
+  def initialize nodes = nil, kaeri = nil
+    @kaeri = kaeri
+    if nodes.nil?
+      @nodes = []
+    else
+      @nodes = [nodes].flatten
+    end
+  end
+  attr_reader :nodes, :kaeri
+
+  def << node
+    @nodes << node
+  end
+
+  def each
+    @nodes.each do |node|
+      if node.is_a? Sequence
+        node.each do |n|
+          yield n
+        end
+      else
+        yield node
+      end
+    end
+  end
+end
+
 class Kakikudashi
   def conv genbun
-    kunten_nodes = kuntens genbun
-    kunten_nodes = punc_hyphen kunten_nodes
-    seq = kakikudashi kunten_nodes
-    seq.join(" / ")
+    @kunten_nodes = get_kunten_nodes genbun
+    # kunten_nodes = punc_hyphen kunten_nodes
+    seq = build_sequence
     kaki = []
     seq.each_with_index{|node, i|
       node.read
@@ -110,84 +136,105 @@ class Kakikudashi
     kaki.join
   end
 
-  def kuntens genbun
-    i = 0
+  def get_kunten_nodes genbun
+    i = -1
     genbun.split(/\s+/).map{|node|
       i += 1
       Kunten.new node, i
     }
   end
 
-  def punc_hyphen kunten_nodes
-    arr = []
-    i = 0
-    index = 0
-    while i < kunten_nodes.size
-      node = kunten_nodes[i]
-      if node.kaeri =~ /^\|/
-        raise node.to_s + kunten_nodes[i+1].to_s unless kunten_nodes[i+1].kaeri.empty?
-        kanji = node.kanji + kunten_nodes[i+1].kanji
-        kaeri = node.kaeri.sub(/\|/, "")
-        kana = kunten_nodes[i+1].kana
-        arr << Kunten.new("#{kanji}#{kana}[#{kaeri}]", index)
-        i += 1 # skip next node
-      else
-        arr << Kunten.new(node.to_raw, index)
-      end
-      i += 1
-      index += 1
+  def build_sequence
+    begin
+      @sequence = Sequence.new
+      @re_ten = {}
+      @ni_ten_stack = []
+      proc_node 0
+      return @sequence
+    rescue => e
+      warn sequence_str
+      raise e
     end
-    return arr
   end
 
-  def kakikudashi kunten_nodes
-    @sequence = []
-    @re_ten = []
-    ni_ten = []
-    jo_ten = []
-    ko_ten = []
-    kunten_nodes.each_with_index do |node, i|
-      case node.kaeri
-      when "レ"
-        @re_ten << node
-        push_sequence node if node.is_saidoku
-      when "二", "三"
-        ni_ten << node
-      when "一"
+  def proc_node i
+    node = @kunten_nodes[i]
+    return if node.nil?
+
+    case node.kaeri
+    when "レ"
+      raise if @kunten_nodes[i+1].nil?
+      @re_ten[node.seq] = node
+      push_sequence node if node.is_saidoku
+    when /^[二三四中下乙丙丁地人]$/
+      @ni_ten_stack << node
+    when /^[一上甲天]$/
+      push_sequence node
+      flush_ni_ten node.kaeri
+    when "一レ", "上レ", "甲レ", "天レ"
+      next_node = @kunten_nodes[i+1]
+      raise if next_node.nil?
+      if next_node.kaeri.empty?
+        push_sequence next_node
         push_sequence node
-        flush ni_ten
-      when "中", "下"
-        jo_ten << node
-      when "上"
-        push_sequence node
-        flush jo_ten
-      when "乙", "丙", "丁"
-        ko_ten << node
-      when "甲"
-        push_sequence node
-        flush ko_ten
-      when "一レ", "上レ", "甲レ"
-        @re_ten << node
-        raise unless kunten_nodes[i+1]
-        kunten_nodes[i+1].kaeri = node.kaeri.sub(/レ/,"")
+        flush_ni_ten node.kaeri
+        proc_node i+2
+        return
       else
-        push_sequence node
+        # todo
       end
+    when /^\|(.)$/
+      kaeri = $1
+      next_node = @kunten_nodes[i+1]
+      raise if next_node.nil?
+      seq = Sequence.new([node, next_node], kaeri)
+      case kaeri
+      when /^[二三四中下乙丙丁地人]$/
+        @ni_ten_stack << seq
+        proc_node i+2
+      when /^[一上甲天]$/
+        push_sequence seq
+        flush_ni_ten kaeri
+        proc_node i+2
+      else
+        raise
+      end
+      return
+    else
+      push_sequence node
     end
-    return @sequence
+    proc_node i+1
+  end
+
+  def flush_ni_ten series
+    re_series = case series
+                when /一/; /[二三四]/
+                when /上/; /[中下]/
+                when /甲/; /[乙丙丁]/
+                when /天/; /[地人]/
+                end
+    until @ni_ten_stack.empty?
+      return unless @ni_ten_stack.last.kaeri =~ re_series
+      push_sequence @ni_ten_stack.pop
+    end
   end
 
   def push_sequence node
+    if node.is_a? Sequence
+      node.nodes.each do |n|
+        push_sequence n
+      end
+      return
+    end
     @sequence << node
-    push_sequence @re_ten.pop if !@re_ten.empty? and @re_ten.last.seq == node.seq-1
+    if @re_ten.key? node.seq-1
+      push_sequence @re_ten[node.seq-1]
+      @re_ten.delete node.seq-1
+    end
   end
 
-  def flush ten_stack
-    if ten_stack.empty?
-      seq = @sequence.map{|node| node.to_s}.join(" - ")
-      raise "stack empty:#{seq}"
-    end
-    push_sequence ten_stack.pop until ten_stack.empty?
+  def sequence_str
+    @sequence.map{|node| node.to_s}.join(" - ")
   end
 end
 
